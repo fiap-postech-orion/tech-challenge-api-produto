fc931c0a685197bb24f49daf59b73def94dd5cb9
Pietro <victor_pietro@hotmail.com>
2024-05-19 21:13:20 -0300

    Mais algumas changes

--- src/main/java/br/com/postech/software/architecture/techchallenge/produto/ProdutoApplication.java
+++ src/main/java/br/com/postech/software/architecture/techchallenge/produto/ProdutoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TechChallengeApplication {
+public class ProdutoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TechChallengeApplication.class, args);
+		SpringApplication.run(ProdutoApplication.class, args);
 	}
 
 }
