88947f50f591de89190682f2e26050a41c22c49a
Yuri Nascimento <yuri.nascimento.ro@gmail.com>
2024-07-07 16:06:15 -0300

    ajustes constantes

---
+++ src/main/java/br/com/postech/software/architecture/techchallenge/produto/util/Constantes.java
@@ -0,0 +1,6 @@
+package br.com.postech.software.architecture.techchallenge.produto.util;
+
+public class Constantes {
+    public static final String ENUM_PACKAGE 							= "br.com.postech.software.architecture.techchallenge.produto.enums.";
+    public static final String ENUM_CLASS_NAME 							= "enumClassName";
+}
